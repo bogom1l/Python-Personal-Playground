9bf817925548058c0ab2fe6c5d8bedf0538923f6
bogom1l <bogomil.stoev01@gmail.com>
2024-03-09 14:57:16 +0200

    Basic python exercises

--- S-D C Course/Intro1/main.py
+++ S-D C Course/Intro1/main.py
@@ -1,14 +1,53 @@
 
 # age, name, gender = 25, 'Jane', 'Female'
 # print(age, name, gender)
+#
+# blood_type = 'AB'
+# print(blood_type)
+#
+# age = 25
+# print(age)
+#
+# age += 5
+# print(age)
+#
+# age -= 5
+# print(age)
 
-blood_type = 'AB'
-print(blood_type)
+# x = 7
+# y = 3
+# z = x/y
+# print(z)
+# print(type(x))
+# print(type(z))
+# print(type(int(7/3.5)))
+#
+# w = int(3.7)
+# print(w)
+# print(type(w))
+#
+# v = float(4)
+# print(v)
+# print(type(v))
 
+# dialogue = 'alex said "hello"'
+# print(dialogue)
 
+# segment_one = "I\'am 25"
+# segment_two = "years old"
+# full_sentence = segment_one + " " + segment_two
+# print(full_sentence)
+# print(len(full_sentence))
+#
+# this_crash_course_is_awesome = True
+# this_crash_course_is_not_awesome = False
+#
+# comparison_operation = not 5 > 3
+# print(comparison_operation)
 
-
-
-
-
+movie_title = 'Harry Potter and the Prisoner of Askaban'
+print(movie_title)
+print(movie_title.upper())
+print(movie_title.lower())
+print(movie_title.count('a'))
 

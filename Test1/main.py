d2350ff2455ab814a975d7b8273534428c11b7ea
bogom1l <bogomil.stoev01@gmail.com>
2024-03-09 12:48:08 +0200

    renamed project

--- Test1/main.py
+++ Test1/main.py
@@ -7,9 +7,19 @@ while numbers:
     print(last_element, end=' ')
 """
 
+"""
+#reverse numbers v2
+numbers = input().split()
+reversed_array = []
 
+while numbers:
+    last_element = numbers.pop()
+    reversed_array.append(last_element)
 
+print(reversed_array)
+"""
 
+print(1)
 
 
 

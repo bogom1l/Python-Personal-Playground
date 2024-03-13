f173c726b049872bad234eb6026ea9ca4b115e9d
bogom1l <bogomil.stoev01@gmail.com>
2024-03-13 17:12:47 +0200

    basic numpy

--- S-D C Course/Intro4-NumPy/main.py
+++ S-D C Course/Intro4-NumPy/main.py
@@ -1,7 +1,45 @@
+import numpy as np
 
+# list_two = list(range(1, 4))
+# list_three = list(range(1, 4))
+# list_sum = []
+#
+# print(list_two)
+# print(list_three)
+#
+# for i in range(3):
+#     list_two[i] = list_two[i] ** 2
+#     list_three[i] = list_three[i] ** 3
+#     list_sum.append((list_two[i] + list_three[i]))
+#
+# print("After:")
+# print(list_two)
+# print(list_three)
+# print(list_sum)
 
+# array1 = np.arange(1, 4)
+# print(array1)
+#
+# array2 = np.arange(1, 4) ** 2
+# print(array2)
+#
+# array3 = np.arange(1, 4) ** 3
+# print(array3)
+#
+# print(array2 + array3)
 
-print('hi)')
+# sample_array = np.array([1, 2, 3])
+#
+# arr = np.power(sample_array, 4)
+# print(arr)
+#
+# arr2 = np.negative(sample_array)
+# print(arr2)
+#
+# print(np.exp(sample_array))
+#
+# print(np.log(sample_array))
+
 
 
 

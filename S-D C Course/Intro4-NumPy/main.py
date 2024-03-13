a49667da770aa892866d2c6b26a45b254949cb59
bogom1l <bogomil.stoev01@gmail.com>
2024-03-13 17:32:55 +0200

    2d arrays

--- S-D C Course/Intro4-NumPy/main.py
+++ S-D C Course/Intro4-NumPy/main.py
@@ -41,6 +41,66 @@ import numpy as np
 # print(np.log(sample_array))
 
 
+##-------- Multidimensional arrays --------
+
+# x = np.arange(3)
+# y = np.arange(3)
+# z = np.arange(3)
+#
+# multi_array = np.array([x, y, z])
+# print(multi_array)
+# print(multi_array.shape)
+#
+# w = np.linspace(0, 20, 11, False)
+# print(w)
+#
+# b = np.arange(1, 30, 3)
+# print(b)
+
+
+# x = np.arange(3)
+# y = np.arange(3, 6)
+# z = np.arange(6, 9)
+#
+# multi_array = np.array([x, y, z])
+# print(multi_array)
+#
+# print(multi_array[0, 0])
+# print(multi_array[1, 2])
+#
+# print(multi_array.dtype)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~ simple exercises ~~~~~~~~~~~~~~~~~~~~~~~~
+
+# # 1.Create a 3x3 matrix with values ranging from 1 to 9
+# matrix = np.arange(1, 10).reshape(3, 3)
+# print(matrix)
+#
+# # 2.Given matrix, extract the second column
+# matrix = np.array([[1, 2, 3],
+#                    [4, 5, 6],
+#                    [7, 8, 9]])
+#
+# second_column = matrix[:, 1]
+# print(second_column)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# Slicing
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

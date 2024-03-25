09501d3a4fa847333b59aee057419790de1d6bd3
bogom1l <bogomil.stoev01@gmail.com>
2024-03-25 12:22:00 +0200

    list comprehension

--- SimpleExercisesV1/main.py
+++ SimpleExercisesV1/main.py
@@ -19,6 +19,15 @@ while numbers:
 print(reversed_array)
 """
 
+"""
 #lambda
 calc = lambda num: "Even number" if num % 2 == 0 else "Odd number"
 print(calc(12))
+"""
+
+#var = [(x, y) for x in range(1, 4) for y in range(10, 14)]
+#var = [x for x in range(10) if x % 2 == 0]
+var = {x: x**2 for x in range(5)}
+
+
+print(var)

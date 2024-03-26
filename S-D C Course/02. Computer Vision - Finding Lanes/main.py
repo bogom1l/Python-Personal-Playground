0637718ff0f3ab9815a2188145f9b23d44646631
bogom1l <bogomil.stoev01@gmail.com>
2024-03-26 14:46:40 +0200

    fixed inconsistency

--- S-D C Course/02. Computer Vision - Finding Lanes/main.py
+++ S-D C Course/02. Computer Vision - Finding Lanes/main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def canny(image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
@@ -30,8 +30,8 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
                             np.array([]), minLineLength=40,
                             maxLineGap=5)

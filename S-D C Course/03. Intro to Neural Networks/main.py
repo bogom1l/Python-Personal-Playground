2ff7cc18c074ede8dcb8e3dbc980b2fb23cf76bc
bogomilstoev7 <bogomil.stoev01@gmail.com>
2024-04-28 21:03:31 +0300

    generated simple scattered points

--- S-D C Course/03. Intro to Neural Networks/main.py
+++ S-D C Course/03. Intro to Neural Networks/main.py
@@ -1,9 +1,17 @@
-
-print(123)
-
 #classification
 #linear model
 #perceptions
 #weights
+####################################################
 
-#TODO: repeat lectures, were too hard
+import numpy as np
+import matplotlib.pyplot as plt
+
+number_of_points = 100
+np.random.seed(0) # every time generates same random numbers
+top_region = np.array([np.random.normal(10, 2, number_of_points), np.random.normal(12, 2, number_of_points)]).T
+bottom_region = np.array([np.random.normal(5, 2, number_of_points), np.random.normal(6, 2, number_of_points)]).T
+_, ax = plt.subplots(figsize=(4, 4))
+ax.scatter(top_region[:, 0], top_region[:, 1], color='r')
+ax.scatter(bottom_region[:, 0], top_region[:, 1], color='b')
+plt.show()
